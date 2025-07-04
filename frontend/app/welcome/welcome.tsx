73a43cb82746774e00b0dc64e94950f1335bf09b
haithamaleryani <haithamaleryani00@gmail.com>
2025-07-04 20:37:31 +0300

    add button

--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -1,7 +1,12 @@
+import { Button } from "~/components/ui/button";
+
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
+    <main className="flex items-center justify-around pt-16 pb-4">
       <p>Hi </p>
+      <Button variant={"outline"} className="cursor-pointer">
+        HI there
+      </Button>
     </main>
   );
 }
