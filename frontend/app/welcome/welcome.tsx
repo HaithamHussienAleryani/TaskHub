0361864c109ec307e29d905b3d6e87db6907b3be
haithamaleryani <haithamaleryani00@gmail.com>
2025-07-05 15:44:34 +0300

    edit

--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -3,7 +3,7 @@ import { Button } from "~/components/ui/button";
 export function Welcome() {
   return (
     <main className="flex items-center justify-around pt-16 pb-4">
-      <p>Hi </p>
+      <p>Hi there</p>
       <Button variant={"outline"} className="cursor-pointer">
         HI there i am haitham
       </Button>
