feef331e7be5bedf9f95dde1cd63c01a0f761e0f
haithamaleryani <haithamaleryani00@gmail.com>
2025-07-10 14:25:11 +0300

    config backend

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,52 @@
 import cors from "cors";
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import morgan  from "morgan"
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express()
+
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    methods:["GET","POST","PUT","DELETE"],
+    allowedHeaders:["Content-Type","Authorization"]
+}))
+app.use(morgan("dev"))
+// add db connection
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log("Mongo DB is connected")
+}).catch((err)=>{
+    console.log("Mongo DB is not connected ", err)
+})
+app.use(express.json())
+
+app.get("/",async (request,response)=>{
+    response.status(200).json({
+        message:"Welcome to Haitham API"
+    })
+})
+
+
+// error not found
+app.use((request,response,next)=>{
+    response.status(404).json({
+        message:"API Endpoint Not Found"
+    })
+})
+
+
+// error middleware 
+app.use((error,request,response,next)=>{
+    console.error(error.stack)
+    response.status(error.status).json(error.message||"Internal Server Error")
+})
+    
+
+app.listen(PORT,()=>{
+    console.log("server is running on port ", PORT)
+})
+
